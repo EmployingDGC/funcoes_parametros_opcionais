67a587fdb5c9652055d860088c30890ac58df3de
EmployingDGC <63275906+EmployingDGC@users.noreply.github.com>
2020-04-25 17:52:36 -0300

    ok

---
+++ bin/main.dart
@@ -0,0 +1,10 @@
+void main(List<String> arguments) {
+  criarBotao('ok');
+}
+
+void criarBotao(String texto, {String cor = 'preto', double largura = 10.0, double altura = 5.0}) {
+  print(texto);
+  print(cor);
+  print(largura);
+  print(altura);
+}
